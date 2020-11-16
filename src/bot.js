ead9a24bc4e8dc597ea70e4de95572a3b8945f11
lucasliano <lliano@est.frba.utn.edu.ar>
2020-11-16 01:40:38 -0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -82,6 +82,7 @@ module.exports = {
 
 function validURL(str)
 {
+  console.log(str);
   var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
             '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
             '((\\d{1,3}\\.){3}\\d{1,3}))'+ // ip (v4) address
