e979849c7c183148b343a7fe9c685a4ece4eb808
lucasliano <lliano@est.frba.utn.edu.ar>
2020-11-16 01:38:58 -0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -82,12 +82,11 @@ module.exports = {
 
 function validURL(str)
 {
-  console.log('Gordo');
   var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
             '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
             '((\\d{1,3}\\.){3}\\d{1,3}))'+ // ip (v4) address
             '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ //port
             '(\\?[;&amp;a-z\\d%_.~+=-]*)?'+ // query string
             '(\\#[-a-z\\d_]*)?$','i');
-  return pattern.test(myURL);
+  return pattern.test(str);
 }
