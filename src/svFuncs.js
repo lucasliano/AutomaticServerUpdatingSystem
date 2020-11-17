91fcdd3014718a79a62df31f07156006993ad77c
lucasliano <lliano@est.frba.utn.edu.ar>
2020-11-16 23:38:04 -0300

    Update svFuncs.js

--- src/svFuncs.js
+++ src/svFuncs.js
@@ -7,12 +7,12 @@ module.exports = {
   {
     try{
       cmd.run('sudo chmod -R 777 ./');
-      cmd.run('cd myServer');
-      cmd.run('git fetch');
-      cmd.run('git reset --hard');
-      cmd.run('git pull --force');
+      cmd.run('git -C ./myServer fetch');
+      cmd.run('git -C ./myServer reset --hard');
+      cmd.run('git -C ./myServer pull --force');
       console.log("> [GIT] Updated with origin/master");
       var PID = run(cmd);
+      console.log("> [PID] The main.js process is running on "+PID);
       return PID;
     } catch (error)
     {
